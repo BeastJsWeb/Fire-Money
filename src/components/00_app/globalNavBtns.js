4d8240c9dfdd3e1c8b2536137329811a98dca7db
Vitaliy Ivanov <vitalijivanov1832@gmail.com>
2022-05-21 01:21:33 +0700

    css nav fixed. nav functional edded

--- src/components/00_app/globalNavBtns.js
+++ src/components/00_app/globalNavBtns.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './global.scss'
 
 export default function GlobalNavBtns (
   {data, className, id}
   ) {
+
+    const [ NavOpen, setNavOpen ] = useState()
+    const navClose = () => { 
+      setNavOpen(document.getElementById('nav--onClick').checked = false)
+    }
+
   return (
-    <div id={id} className={className} >
+    <div 
+    id={id} 
+    className={className} 
+    onClick={navClose} 
+    open={NavOpen} 
+    >
       {data.map(({name, ancor}) => (
         <a href={ancor} key={name} >
           {name}
